07af0dbdad5343593c90b878afd11a00fbfa47be
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-06-02 11:35:15 +0530

    Create routes

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
+
+class App extends React.Component {
+  render () {
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Greeting/>}/>
+          </Routes>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,5 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+const Greeting = () => {
+  return (
+    <h1>Greeting!</h1>
+  );
+}
+/*
 class Greeting extends React.Component {
   render () {
     return (
@@ -13,4 +20,6 @@ class Greeting extends React.Component {
 Greeting.propTypes = {
   message: PropTypes.node
 };
-export default Greeting
+*/
+
+export default Greeting;
