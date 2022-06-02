67ec2a0dcf0c2dc3fd25ff55ec1fdc1814726e9c
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-06-02 16:25:04 +0530

    Add linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::GreetingsController < ApplicationController
+  def index
+    @random = Greeting.all.sample(1).first
+    render json: {
+      message: @random.message
+    }
+  end
 
-	def index
-		@random = Greeting.all.sample(1).first
-		render json: {
-			message: @random.message
-		}
-	end
-
-	def show
-	end
+  def show; end
 end
